c5fc768c6b3eb4d02bd639f31c183d59333ee68b
ajit <sajit4862@gmail.com>
2022-07-15 16:28:36 +0530

    updates for eb

--- Service_Portal/Service_Portal/settings.py
+++ Service_Portal/Service_Portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#w)%48nkd40z=n*u#5%$&nb%2*$knd9ba_$orcv&7=iq3ukfo9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','django-env.eba-bmwksvmm.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1','rockstar-env.eba-kwbuphj7.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
