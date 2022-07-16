7f20d17be916c809abfa12c52ddad7281306386b
ajit <sajit4862@gmail.com>
2022-07-16 15:23:39 +0530

    updated

--- Service_Portal/Model/admin.py
+++ Service_Portal/Model/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Oem,Specification,Dealer,Battery,Client
 
+class BatteryAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    list_display=('serial_no','specification')
+
 
 
 admin.site.register(Oem)
