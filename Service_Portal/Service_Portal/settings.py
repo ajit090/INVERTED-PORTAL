69bf56590487f534c6a1757876665073afbe3e0f
ajit <sajit4862@gmail.com>
2022-07-18 19:04:08 +0530

    updated

--- Service_Portal/Service_Portal/settings.py
+++ Service_Portal/Service_Portal/settings.py
@@ -74,13 +74,19 @@ WSGI_APPLICATION = 'Service_Portal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Ajit12',
+        'PASSWORD': 'Ajit7595',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+         }
+            }
+}
 
 
 # Password validation
